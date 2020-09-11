62b1376192b70a02a4eeece03d6219c4d19f7711
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-11 19:03:20 -0300

    products controllers done

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -11,12 +11,17 @@ export const getProducts=async (req,res)=>{
     const products= await product.find()
     res.status(200).json(products)
 }
-export const getProductById=(req,res)=>{
-
+export const getProductById=async (req,res)=>{
+    const prod= await product.findById(req.params.productId)
+    res.status(200).json(prod)
 }
-export const updateProductById=(req,res)=>{
-
+export const updateProductById=async (req,res)=>{
+    const updatedProduct= await product.findByIdAndUpdate(req.params.productId,req.body,{
+         new: true
+        })
+    res.status(200).json(updatedProduct)
 }
-export const deleteProductById=(req,res)=>{
-
+export const deleteProductById=async(req,res)=>{
+    const deletedProduct= await product.findByIdAndDelete(req.params.productId)
+    res.status(204).json()
 }
